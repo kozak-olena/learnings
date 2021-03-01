cba7329cbc10c40a1e136aa2be7faeeef9940606
Olena Kozak <kozak.olena11@gmail.com>
2021-03-01 10:48:15 +0200

    added ArrayList.cs

---
+++ ArrayList/ArrayList/ArrayList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList
+{
+    class ArrayList
+    {
+        private int growthCoeficient = 2;
+        private int initialSize = 3;
+        private int index = 0;
+        private int[] _array;
+
+        public ArrayList()
+        {
+            int[] newArray = new int[initialSize];
+            _array = newArray;
+        }
+
+        public void Add(int value)
+        {
+            if (index < _array.Length)
+            {
+                _array[index] = value;
+                index++;
+            }
+            else
+            {
+                int newSize = _array.Length * growthCoeficient;
+                int[] newArray = new int[newSize];
+                for (int i = 0; i < _array.Length; i++)
+                {
+                    newArray[i] = _array[i];
+                }
+                newArray[_array.Length] = value;
+                index++;
+                _array = newArray;
+
+            }
+        }
+
+        public void RemoveAt(int indextOfElement)
+        {
+
+            for (int i = indextOfElement; i < index; i++)
+            {
+ 
+                _array[i] = _array[i + 1];
+
+            }
+
+            index--;
+
+        }
+    }
+}
---
+++ ArrayList/ArrayList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.Add(23);
+            arrayList.Add(12);
+            arrayList.Add(34);
+            arrayList.Add(10);
+            arrayList.Add(14);
+            arrayList.Add(334);
+
+            arrayList.Add(100);
+
+            arrayList.RemoveAt(4);
+            arrayList.RemoveAt(6);
+        }
+    }
+}
